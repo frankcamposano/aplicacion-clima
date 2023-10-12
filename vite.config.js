6ef6d414bbb38ad4a37b9d3294dd73a62b623c8d
frankcamposano <fcamposano25@gmail.com>
2023-10-12 14:49:03 -0500

    aea

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,9 @@ export default defineConfig({
   plugins: [
     react(),
     replace({
+      preventAssignment: true,
       // Define tus variables de entorno y sus valores aquí
-      'process.env.REACT_APP_URL': JSON.stringify('http://api.weatherapi.com/v1/current.json?aqi=no'),
+      'process.env.REACT_APP_URL': JSON.stringify('https://api.weatherapi.com/v1/current.json?aqi=no'),
       'process.env.REACT_APP_KEY': JSON.stringify('7fb895cb9a8f4e0d8d6184704231509'),
     })
   
